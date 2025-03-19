5bcff709287d3c73030002e7b4400f4f09dbd8bc
EliOA99 <elisha-oa@outlook.com>
2025-03-19 13:29:20 +0000

    Code cleanup Hover animation adjusted Research how to implement highlighted on my site

---
+++ scripts/activePage.js
@@ -0,0 +1,13 @@
+let marker = document.querySelector('#marker');
+let item = document.querySelector('pageNav');
+
+function indicator(e){
+    marker.style.left = e.offsetLeft+'px';
+    marker.style.width = e.offsetWidth+'px';
+}
+
+item.forEach(link =>{
+    link.addEventListener('click', (e) => {
+        indicator(e.target);
+    })
+})
