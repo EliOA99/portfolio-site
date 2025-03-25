4fb18f0c032d8e289b535359855f81510e67c9ee
EliOA99 <elisha-oa@outlook.com>
2025-03-25 10:46:19 +0000

    Overlapping issues sorted Working on page content

--- scripts/activePage.js
+++ scripts/activePage.js
@@ -1,5 +1,5 @@
 const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.BLinks a:not(.dropdown-content a)').forEach(link => {
+const navLinks = document.querySelectorAll('.BLinks a').forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.classList.add('active');
     console.log(link);
